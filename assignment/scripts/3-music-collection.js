ab6ece838a568afb547a31e973580c78accb885d
Katie Stairs <kstairs831@gmail.com>
2022-11-22 15:21:18 -0600

    Started object and function work

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+    album1.push(collection);
+    return collection;
+    console.log(collection);
+}
+
+const album1 = {
+    title: "Texas Flood",
+    artist: "Stevie Ray Vaughn",
+    yearPublished: 1983
+};
+
